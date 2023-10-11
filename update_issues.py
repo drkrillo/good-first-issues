7d55a9f1365de8e0ff49b1eb2580a74f587ea42a
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-10-11 01:05:46 -0300

    debugged extract_repos func

--- update_issues.py
+++ update_issues.py
@@ -68,11 +68,11 @@ async def extract_issues(repo, session):
 
 async def extract_repos(user, session):
     user_url = f"https://api.github.com/users/{user}/repos"
-            
+    repos = [] 
     async with session.get(user_url) as resp:
         user = await resp.json()
         try:
-            repos = [x['url'] for x in user]
+            repos += [x['url'] for x in user]
         except:
             pass
     return repos
