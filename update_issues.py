8f9956dea8a17ae72f12e4beac2569af45b064c5
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-10-11 17:34:22 -0300

    debugged extract_n umber_of_repos func

--- update_issues.py
+++ update_issues.py
@@ -79,7 +79,7 @@ async def extract_issues(repo, session):
 async def extract_number_of_repos(user, session):
     user_url = f"https://api.github.com/users/{user}/"
     async with session.get(user_url) as resp:
-        resp = await.resp.json()
+        resp = await resp.json()
         total_repos = resp['public_repos']
     return total_repos
 
