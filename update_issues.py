62ce7d533111540c0311d103457d02cf1c8433f5
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-10-11 01:35:18 -0300

    changed names of scripts

--- update_issues.py
+++ update_issues.py
@@ -1,15 +1,17 @@
 import os
 import datetime
 from dotenv import load_dotenv
-import asyncio, aiohttp
+
+from github import Github, Auth
+
 from jinja2 import Environment, FileSystemLoader
+
 
 load_dotenv()
 access_token = os.environ.get('ACCESS_TOKEN')
 
 usernames = [
     "pandas-dev",
-    'numpy',
     'django',
     'flask',
     'fastapi',
@@ -17,6 +19,7 @@ usernames = [
     'tensorflow'
     'pytorch',
     'opencv',
+    'CNTK',
     'includeos',
     'xbmc',
     'monero-project',
@@ -25,83 +28,49 @@ usernames = [
     'godotengine',
 ]
 
-gathered_users = []
 
-headers = {
-    "Authorization": f" Bearer  {access_token}"
-}
+def main():
+    auth = Auth.Token(access_token)
+    g = Github(auth=auth)
 
-def create_list_from_lists(target_list):
-    list_of_lists = []
-    for element in target_list:
-        if len(element) > 0:
-            for e in element:
-                list_of_lists.append(e)
-    return list_of_lists
-
-def extract_issue_data(raw_issue):
-    issue = {}
-    issue['repo'] = raw_issue[1]['repository_url'].split('repos/')[1]
-    issue['language'] = raw_issue[0]
-    issue['title'] = raw_issue[1]['title']
-    issue['url'] = raw_issue[1]['html_url']
-    issue['comments'] = raw_issue[1]['comments']
-
-    return issue
-
-async def extract_language(repo, session):
-    async with session.get(repo)as resp:
-        repo = await resp.json()
-        language = repo['language']
-    return language
-
-async def extract_issues(repo, session):
-    issues = []
-    language = await extract_language(repo, session)
-    issues_url = repo + "/issues?labels=good first issue"
-    async with session.get(issues_url) as resp:
-        resp = await resp.json()
-        if len(resp) > 0:
-            resp = [(language, r) for r in resp]
-            issues += resp
-    return issues
-
-async def extract_repos(user, session):
-    user_url = f"https://api.github.com/users/{user}/repos"
-    repos = []
-
-    async with session.get(user_url) as resp:
-        user = await resp.json()
+    results = []
+    for username in usernames:
         try:
-            repos += [x['url'] for x in user]
-        except:
-            pass
-    return repos
+            user_data = g.get_user(username)
 
-async def main():
-    async with aiohttp.ClientSession(headers=headers) as session:
-        
-        repos = [extract_repos(user,  session) for user in usernames]
-        repos = await  asyncio.gather(*repos)
-        repos= create_list_from_lists(repos)
+            for repo in user_data.get_repos():
+                open_issues = repo.get_issues(
+                    state='open',
+                    labels=['good first issue'],
+                    since=datetime.datetime(2019, 1, 1, 0, 0)
+                )
 
-        issues = [extract_issues(repo, session) for repo in repos]
-        issues = await asyncio.gather(*issues)
-        issues = create_list_from_lists(issues)
-        print(len(issues))
-        issues = [extract_issue_data(issue) for issue in issues]
-        print(len(issues))
-        issues = sorted(issues, key=lambda x: (x['language'], x['comments']))
+                for issue in open_issues:
+                    result = {
+                        'repo': repo.full_name.split('/')[1],
+                        'language': repo.language,
+                        'title': issue.title,
+                        'comments': str(issue.comments),
+                        'url': issue.url.replace('api.','').replace('repos/',''),
+                    }
+                    results.append(result)
+
+                
+                print('done repo', repo.full_name)
+        except Exception as e:
+            print(e)
+
+        results = sorted(results, key=lambda x: (x['language'], x['comments']))
         
         env = Environment(loader=FileSystemLoader('templates'))
         template = env.get_template('README.md.j2')
 
         today = str(datetime.datetime.today().strftime('%Y-%m-%d'))
 
-        rendered_readme = template.render(results=issues, today=today)
+        rendered_readme = template.render(results=results, today=today)
 
         with open("README.md", "w+") as f:
             f.write(rendered_readme)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    main()
