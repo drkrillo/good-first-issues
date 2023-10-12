9d839f874004b9e841fdbc24d6e6dbe18a3bd074
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-10-12 04:49:51 -0300

    formatted code, added docstrings

--- update_issues.py
+++ update_issues.py
@@ -13,6 +13,7 @@ from jinja2 import Environment, FileSystemLoader
 load_dotenv()
 access_token = os.environ.get('ACCESS_TOKEN')
 
+
 usernames = [
     "pandas-dev",
     'django',
@@ -30,28 +31,39 @@ usernames = [
     'godotengine',
 ]
 
-gathered_users = []
-
 headers = {
     "Authorization": f" Bearer  {access_token}"
 }
 
 today = str(datetime.datetime.today().strftime('%Y-%m-%d'))
 
-def define_number_of_pages(number_of_repos, total_repos_per_page=100):
-    number_of_pages = number_of_repos / total_repos_per_page
-    number_of_pages = math.ceil(number_of_pages)
-    return number_of_pages
 
-def create_list_from_lists(target_list):
+def divide_and_round_up(num: int,  denom: int=100):
+    """
+    Takes two numbers and returns the first biggest int.
+    """
+    total = num / denom
+    total_round_up = math.ceil(total)
+    return total_round_up
+
+def create_list_from_lists(target: list):
+    """
+    Takes a list of lists and 
+    returns a coninuous list with aall elements.
+    """
     list_of_lists = []
-    for element in target_list:
+    for element in target:
         if len(element) > 0:
             for e in element:
                 list_of_lists.append(e)
     return list_of_lists
 
-def extract_issue_data(raw_issue):
+def extract_issue_data(raw_issue: tuple):
+    """
+    Takes an issue (language, issue dict)
+    and returns a depurated dict withissue data.
+
+    """
     issue = {}
     issue['repo'] = raw_issue[1]['repository_url'].split('repos/')[1]
     issue['language'] = raw_issue[0]
@@ -61,15 +73,23 @@ def extract_issue_data(raw_issue):
     return issue
 
 async def extract_language(repo, session):
+    """
+    Takes the repo API endpoint and aiohttp session
+    and returns the principal language of the repository.
+    """
     async with session.get(repo)as resp:
         repo = await resp.json()
         language = repo['language']
     return language
 
-async def extract_issues(repo, session):
+async def extract_issues(repo, session, labels="good first issue"):
+    """
+    Takes the repo API endpoing and aiohttp session
+    and returns issues  with defined labels and state.
+    """
     issues = []
     language = await extract_language(repo, session)
-    issues_url = repo + "/issues?labels=good first issue"
+    issues_url = repo + f"/issues?labels={labels}"
     async with session.get(issues_url) as resp:
         resp = await resp.json()
         if len(resp) > 0:
@@ -78,6 +98,10 @@ async def extract_issues(repo, session):
     return issues
 
 async def extract_number_of_repos(user, session):
+    """
+    Takes a username and aiohttp sesion and returns
+    thetotal number of open repositories.
+    """
     user_url = f"https://api.github.com/users/{user}"
     async with session.get(user_url) as resp:
         resp = await resp.json()
@@ -89,9 +113,13 @@ async def extract_number_of_repos(user, session):
     return num_repos
 
 async def extract_repos(user, session, repos_per_page=100):
+    """
+    Takes a username, aiohttp sesion and number of repos pr page
+    and returns list of lists of repositories. 
+    """
     repos = []
     number_of_repos = await extract_number_of_repos(user, session)
-    number_of_pages = define_number_of_pages(number_of_repos)
+    number_of_pages = divide_and_round_up(number_of_repos)
     
     for page in range(1,number_of_pages+1):
         user_url = f"https://api.github.com/users/{user}/repos?page={page}&per_page={repos_per_page}"
@@ -104,6 +132,12 @@ async def extract_repos(user, session, repos_per_page=100):
         return repos
 
 async def main():
+    """
+    1- Gathers all issues associated with all public repos
+    in the usernames list defined at the beginning of the scrit.
+
+    2- Updates README.md file.
+    """
     async with aiohttp.ClientSession(headers=headers) as session:
         
         print("Gathering repositories...")
