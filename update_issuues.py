7b7767ec5fd1613bd232228575ad94fef061022b
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-10-12 02:18:45 -0300

    added asynchrnouus script

--- update_issuues.py
+++ update_issuues.py
@@ -1,10 +1,12 @@
 import os
 import datetime
 from dotenv import load_dotenv
+import time
 
-import math
+import math 
 
 import asyncio, aiohttp
+
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -13,12 +15,11 @@ access_token = os.environ.get('ACCESS_TOKEN')
 
 usernames = [
     "pandas-dev",
-    'numpy',
     'django',
     'flask',
     'fastapi',
     'ansible',
-    'tensorflow',
+    'tensorflow'
     'pytorch',
     'opencv',
     'includeos',
@@ -34,7 +35,6 @@ gathered_users = []
 headers = {
     "Authorization": f" Bearer  {access_token}"
 }
-
 
 def define_number_of_pages(number_of_repos, total_repos_per_page=100):
     number_of_pages = number_of_repos / total_repos_per_page
@@ -56,13 +56,12 @@ def extract_issue_data(raw_issue):
     issue['title'] = raw_issue[1]['title']
     issue['url'] = raw_issue[1]['html_url']
     issue['comments'] = raw_issue[1]['comments']
-
     return issue
 
 async def extract_language(repo, session):
-    async with session.get(repo) as resp:
+    async with session.get(repo)as resp:
         repo = await resp.json()
-    language = repo['language']
+        language = repo['language']
     return language
 
 async def extract_issues(repo, session):
@@ -80,26 +79,26 @@ async def extract_number_of_repos(user, session):
     user_url = f"https://api.github.com/users/{user}"
     async with session.get(user_url) as resp:
         resp = await resp.json()
+    try:
+        num_repos = resp['public_repos']
+    except:
+        num_repos = 100
+    print(num_repos)
+    return num_repos
 
-    total_repos = resp['public_repos']
-    return total_repos
-
-async def extract_repos(user, session, number_of_repos_per_page=100):
+async def extract_repos(user, session, repos_per_page=100):
     repos = []
     number_of_repos = await extract_number_of_repos(user, session)
     number_of_pages = define_number_of_pages(number_of_repos)
-    print(f"Started extracting repos for {user}")
+    
     for page in range(1,number_of_pages+1):
-        user_url = f"https://api.github.com/users/{user}/repos?page={page}&per_page={number_of_repos_per_page}"
+        user_url = f"https://api.github.com/users/{user}/repos?page={page}&per_page={repos_per_page}"
 
         async with session.get(user_url) as resp:
-            repos = await resp.json()
-            try:
-                repos += [x['url'] for x in repos]
-            except:
-                    pass
-    print(f"Done repos for user {user}")
-    return repos
+            user = await resp.json()
+            if type(user) == list:
+                repos += [x['url'] for x in user]
+        return repos
 
 async def main():
     async with aiohttp.ClientSession(headers=headers) as session:
@@ -107,23 +106,25 @@ async def main():
         repos = [extract_repos(user,  session) for user in usernames]
         repos = await  asyncio.gather(*repos)
         repos= create_list_from_lists(repos)
-
+        
         issues = [extract_issues(repo, session) for repo in repos]
         issues = await asyncio.gather(*issues)
-        issues = create_list_from_lists(issues) 
+        issues = create_list_from_lists(issues)
 
         issues = [extract_issue_data(issue) for issue in issues]
-
         issues = sorted(issues, key=lambda x: (x['language'], x['comments']))
+        
+        print(f"Total Issues gathered: {len(issues)}")
         env = Environment(loader=FileSystemLoader('templates'))
         template = env.get_template('README.md.j2')
-
         today = str(datetime.datetime.today().strftime('%Y-%m-%d'))
-
         rendered_readme = template.render(results=issues, today=today)
-
+        
         with open("README.md", "w+") as f:
             f.write(rendered_readme)
 
 if __name__ == '__main__':
+    start_time = time.perf_counter() 
     asyncio.run(main())
+    end_time = time.perf_counter()
+    print(f"Scrpint runtine: {end_time - start_time}")
