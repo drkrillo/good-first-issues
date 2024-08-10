7a859bb392168876acf8b9401ecd72e304507405
Juan Pasutti <juan.pasutti@fluxit.com.ar>
2024-08-10 00:52:15 -0300

    Adds logging for error in custom exception

--- exception_handler.py
+++ exception_handler.py
@@ -1,10 +1,30 @@
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
 class APIError(Exception):
     """
     Exception Handling for API Rate Limit Ecxceeded.
     
     Input:
+    status: Status code of the API call.
     message: The eerror message passed to the exception.
     """
     def __init__(self, status, message):
-        custom_message = f"Status: {status}. Message: {message}"
-        super().__init__(custom_message)
+        self.status = status
+        self.message = message
+        self.custom_message = f"APIError {self.status}: {message}"
+        
+        logging.error(self.custom_message)
+        
+        super().__init__(self.message)
+        
+    def __str__(self):
+        return self.custom_message
