953a3c70b2e2947fea8d1d33fd162766a3c2ea25
Juan Pasutti <juan.pasutti@fluxit.com.ar>
2024-09-09 21:06:44 -0300

    Changed framework to pytest

--- app/tests/test_api_handler.py
+++ app/tests/test_api_handler.py
@@ -1,38 +1,27 @@
-import unittest
-from app.api_handler import APIHandler 
+import pytest
+from app.api_handler import (
+    APIClient,
+    RepoManager,
+    IssueManager,
+    Utils,
+)
 from unittest.mock import Mock, patch
 import requests
 
-class TestAPIHandler(unittest.TestCase):
+class TestUtils:
 
-    def setUp(self):
-        self.api_handler = APIHandler()
+    def test_utils_divide_and_round_up(self):
+        result = Utils().divide_and_round_up(150, 100)
+        assert result == 2
 
-    def test_divide_and_round_up(self):
-        result = self.api_handler.divide_and_round_up(150, 100)
-        self.assertEqual(result, 2)
-
-    def test_create_list_from_lists(self):
+    def test_utils_create_list_from_lists(self):
         nested_list = [[1, 2, 3], [4, 5, 6], None, [7, 8]]
-        result = self.api_handler.create_list_from_lists(nested_list)
-        self.assertEqual(result, [1, 2, 3, 4, 5, 6, 7, 8])
+        result = Utils().create_list_from_lists(nested_list)
+        assert result == [1, 2, 3, 4, 5, 6, 7, 8]
 
-    def test_extract_issue_data(self):
-        raw_issue = ('Python', {
-            'repository_url': 'https://api.github.com/repos/python/cpython',
-            'title': 'Example issue',
-            'html_url': 'https://github.com/python/cpython/issues/1',
-            'comments': 5
-        })
-        result = self.api_handler.extract_issue_data(raw_issue)
-        self.assertEqual(result['repo'], 'python/cpython')
-        self.assertEqual(result['language'], 'Python')
-        self.assertEqual(result['title'], 'Example issue')
-        self.assertEqual(result['url'], 'https://github.com/python/cpython/issues/1')
-        self.assertEqual(result['comments'], 5)
-    
+class TestAPIClient:
     @patch('app.api_handler.requests.Session.get')
-    def test_make_request_success(self, mock_get):
+    def test_apiclient_make_request_success(self, mock_get):
         mock_response = Mock()
         mock_response.raise_for_status.return_value = None
         mock_response.json.return_value = {
@@ -44,5 +33,9 @@ class TestAPIHandler(unittest.TestCase):
         url = 'http://www.mock-url.com'
         result = APIHandler()._make_request(url, session)
         
-        self.assertEqual(result, {'mocked_key': 'mocked_value'})
+        assert result == {'mocked_key': 'mocked_value'}
+    
+    @patch('app.api_handler.requests.Session.get')
+    def test_apiclient_make_request_error(self, mock_get):
+        pass
 
