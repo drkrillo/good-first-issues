6b341d4bccfcce0f324adcc8fdc1ff9cd59e5aee
achan208 <anushka.chandel1@aexp.com>
2026-08-02 12:10:33 +0530

    fix: correct failing testcases

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 import requests
 
+
 @pytest.fixture
 def mock_session(mocker):
     return mocker.Mock(spec=requests.Session)
+
+
+@pytest.fixture
+def mock_env_vars(monkeypatch):
+    monkeypatch.setattr('app.update_issues.HEADERS', {'Authorization': 'Bearer test-token'})
+    monkeypatch.setattr('app.update_issues.USERNAMES', ['test_user'])
